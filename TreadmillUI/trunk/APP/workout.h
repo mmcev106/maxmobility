6eebf3347c335a0519e19ce9644a3a9fed2a6c08
mark@fastmail.net <mark@fastmail.net@9db6a66f-09bd-6529-4fc0-e4370dc81027>
2011-05-14 12:56:32 +0000

    Renamed an argument to fix a copilation error for Chris.

--- TreadmillUI/trunk/APP/workout.h
+++ TreadmillUI/trunk/APP/workout.h
@@ -17,7 +17,7 @@ public:
     static Workout* createWorkout(QString name, float speed, float grade, int minutes);
     static Workout* createDynamicGradeWorkout(QString name, int minutes, int lowGrade , int highGrade, int age, int weight);
     static Workout* createIntensityWorkout(QString name, int minutes, int initialSpeed, double intensityPercent, int weight);
-    static Workout* createDynamicSpeedWorkout(QString name, int minutes, int lowSpeed , int lowSpeed, int age, int weight);
+    static Workout* createDynamicSpeedWorkout(QString name, int minutes, int lowSpeed , int highSpeed, int age, int weight);
     static Workout* createHeartRateWorkout(QString name, int minutes, float lowPercentage , float highPercentage, int age, int weight);
 };
 
