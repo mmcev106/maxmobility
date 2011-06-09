7009afb7a5c339e70aa908f933bf47abc9b5c1f4
beckerwp@gmail.com <beckerwp@gmail.com@9db6a66f-09bd-6529-4fc0-e4370dc81027>
2011-06-09 22:17:50 +0000

    had some trouble trying to remove a file from the SVN - reloaded all the files to make sure it was right. So if you have to re-download everything.. that's ok.

---
+++ TreadmillUI/trunk/APP/C# TreadmillApplication/TreadmillApplication/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Reflection;
+using System.Windows.Interactivity;
+
+namespace TreadmillApplication
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+		}
+	}  
+ 
+	public class ActivateTabAction : TargetedTriggerAction<FrameworkElement>
+	{
+		public static readonly DependencyProperty TabNameProperty = DependencyProperty.Register("TabName", typeof(string), typeof(ActivateTabAction), new PropertyMetadata(string.Empty));
+		
+		public string TabName
+		{
+			get { return (string)GetValue(TabNameProperty); }
+			set { SetValue(TabNameProperty, value); }
+		}
+		
+		protected override void Invoke(object o)
+		{
+			// Find userControl or Window parent.
+			var control = this.Target;
+			
+			while(control != null && !(control is UserControl) && !(control is Window))
+			{
+				control = control.Parent as FrameworkElement;
+			}
+			
+			if(control == null || (!(control is UserControl) && !(control is Window)))
+			{
+				return;
+			}
+			
+			// Find the TabItem field by its name.
+			var type = control.GetType();
+			var target = type.GetField(this.TabName, BindingFlags.IgnoreCase | BindingFlags.NonPublic | BindingFlags.Instance);
+			
+			if(target == null)
+			{
+				return;
+			}
+			
+			// Get the tabitem.
+			var tabItem = target.GetValue(control) as TabItem;
+			if(tabItem == null)
+			{
+				return;
+			}
+			
+			ActivateTab(tabItem);
+			
+			// Recursively search for tab controls up the visual tree to activate.
+			var parent = tabItem.Parent;
+			while (parent != null)
+			{
+				if (parent is TabItem)
+				{
+					ActivateTab(parent as TabItem);
+				}
+				if (parent is FrameworkElement)
+				{
+					parent = (parent as FrameworkElement).Parent;
+				}
+				else
+				{
+					parent = null;
+				}
+			}
+		}
+		
+		// Find the TabItem's parent TabControl and activate the tab.
+		private void ActivateTab(TabItem tabItem)
+		{
+			var tabControl = tabItem.Parent as TabControl;
+			if (tabControl != null)
+			{
+				tabControl.SelectedItem = tabItem;
+			}
+		}
+	}
+}
---
+++ TreadmillUI/trunk/APP/C# TreadmillApplication/TreadmillApplication/Startup.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace TreadmillApplication
+{
+	public partial class Startup
+	{
+		public Startup()
+		{
+			this.InitializeComponent();
+
+			// Insert code required on object creation below this point.
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+			
+		}
+		
+		private void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			Trace.WriteLine("Startup page loaded.");
+		}
+		
+		private void Page_Unloaded(object sender, RoutedEventArgs e)
+		{
+			Trace.WriteLine("Startup page unloaded.");
+		}
+		
+		~Startup()
+		{
+			Trace.WriteLine("Startup page destroyed.");
+		}
+	}
+}
